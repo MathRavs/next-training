a749ea21e26416019bba36016dbf8bcbf2c2a0c4
MoonCypher <mathiasravelojaona@gmail.com>
2024-12-08 00:55:03 +0100

    finish chapter 7

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -6,9 +6,8 @@ import Link from 'next/link';
 import { generatePagination } from '@/app/lib/utils';
 
 export default function Pagination({ totalPages }: { totalPages: number }) {
-  // NOTE: Uncomment this code in Chapter 11
 
-  // const allPages = generatePagination(currentPage, totalPages);
+   //const allPages = generatePagination(currentPage, totalPages);
 
   return (
     <>
